19c558f028ba888ed580e354cba677d36922a1f4
amurthy <am2275@cornell.edu>
2013-07-19 10:44:01 -0700

    Initial Commit

---
+++ Super Tic-Tac-Toe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Super Tic-Tac-Toe
+//
+//  Created by Ashwin Murthy on 7/19/13.
+//  Copyright (c) 2013 Ashwin Murthy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Super Tic-Tac-ToeTests/Super_Tic_Tac_ToeTests.m
@@ -0,0 +1,36 @@
+//
+//  Super_Tic_Tac_ToeTests.m
+//  Super Tic-Tac-ToeTests
+//
+//  Created by Ashwin Murthy on 7/19/13.
+//  Copyright (c) 2013 Ashwin Murthy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Super_Tic_Tac_ToeTests : XCTestCase
+
+@end
+
+@implementation Super_Tic_Tac_ToeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
